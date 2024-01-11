f35d247eabd716c78e8a20a8a6309294b6767861
lionel staricoff <staricofflionel@hotmail.com>
2024-01-11 17:05:30 -0500

    agregando eventos  touch

--- js/main.js
+++ js/main.js
@@ -19,7 +19,19 @@ try {
   buttons.forEach(li => {li.addEventListener('click', (event) => { l.impl.menuClose(event); });});
   menu.addEventListener('click', (event) => {l.impl.menuOpen(event)});
 
- 
+  /* efectos touch */
+  luz.addEventListener('click', () => { l.impl.modoBack() });
+  foto.addEventListener('click', () => { l.impl.clear(); });
+
+  menu.addEventListener('touchstart', (event) => {l.impl.menuOpen(event)});
+  buttons.forEach(li => {li.addEventListener('touchstart', (event) => { l.impl.menuClose(event); });});
+  
+  buttons[0].addEventListener('touchstart', () => { l.impl.js(); });
+  buttons[2].addEventListener('touchstart', () => { l.impl.cv(); });
+  buttons[4].addEventListener('touchstart', () => { l.impl.clases(); });
+  buttons[5].addEventListener('touchstart', () => { l.impl.java(); });
+  buttons[8].addEventListener('touchstart', () => { l.impl.email(); });
+
   /* efecto scroll */
   document.addEventListener("scroll", () => { l.impl.scroll(); });
 } catch (error) {
